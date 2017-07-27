6eace1b96eb6f8a01caed1f8c096089b72dc337d
Shweta Patrachari <shweta@Shwetas-MacBook-Pro.local>
2017-07-27 13:45:29 -0700

    changed functionality and modified styling
    - removed pre-existing arrays and shuffle functions
    - replaced hard code with more flexible functions
    - added capability to add or update student information
    - reversed appearance order of newly drawn raffle tickets
    - adjusted bootstrap styling
    - added media queries for better responsive design
    - added "start over" function to refresh raffle

--- js/main.js
+++ js/main.js
@@ -1,92 +1,101 @@
-
-var array = [];
-// uncomment below to test functionality with numbers (i) in place of names
-// for (i = 0; i < 150; i++){
-//   array[i] = i;
-//   array.push(array[i])
-// }
-
-// // assign student names as values for each position in array for however many raffle tickets they earned
-for (i = 0; i < 150; i++) {
-  // student A had 18 raffle tickets, name goes in 18 times from position i=0 to i=17
-  if (i <= 17) {
-    array[i] = "Student A";
-    // student B had 10 tickets, name goes in 10 times from position i=18 to i=27
-  } else if (i > 17 && i <= 27) {
-    array[i] = "Student B";
-  } else if (i > 27 && i <= 36) {
-    array[i] = "Student C";
-  } else if (i > 36 && i <= 46) {
-    array[i] = "Student D";
-  } else if (i > 46 && i <= 77) {
-    array[i] = "Student E";
-  } else if (i > 77 && i <= 79) {
-    array[i] = "Student F";
-  } else if (i > 79 && i <= 84) {
-    array[i] = "Student G";
-  } else if (i > 84 && i <= 86) {
-    array[i] = "Student H";
-  } else if (i > 86 && i <= 87) {
-    array[i] = "Student I";
-  } else if (i > 87 && i <= 88) {
-    array[i] = "Student J";
-  } else if (i > 88 && i <= 89) {
-    array[i] = "Student K";
-  } else if (i > 89 && i <= 90) {
-    array[i] = "Student L";
-  } else if (i > 90 && i <= 91) {
-    array[i] = "Student M";
-  } else if (i > 91 && i <= 93) {
-    array[i] = "Student N";
-  } else if (i > 93 && i <= 101) {
-    array[i] = "Student O";
-  } else if (i > 101 && i <= 119) {
-    array[i] = "Student P";
-  } else if (i > 119 && i <= 121) {
-    array[i] = "Student Q";
-  } else if (i > 121 && i <= 136) {
-    array[i] = "Student R";
-  } else if (i > 136 && i <= 137) {
-    array[i] = "Student S";
-  } else if (i > 137 && i <= 139) {
-    array[i] = "Student T";
-  } else if (i > 139 && i <= 142) {
-    array[i] = "Student U";
-  } else if (i > 142 && i <= 147) {
-    array[i] = "Student V";
-  } else if (i > 147 && i < 150) {
-    array[i] = "Student W";
+var studentArray = [
+  {
+    firstName : 'Charles',
+    lastName : 'Darwin',
+    stickers: 7,
+    raffleName : 'Charles D.',
+  },
+  {
+    firstName : 'Marie',
+    lastName : 'Curie',
+    stickers: 10,
+    raffleName : 'Marie C.',
   }
-  array.push(array[i])
+];
+var raffleArray;
+
+function renderStudentObject(firstName, lastName, stickers) {
+  return {
+    firstName : firstName,
+    lastName : lastName,
+    stickers: stickers,
+    raffleName : firstName + ' ' + lastName[0] + '.',
+  }
 }
 
-function shuffle(array) {
-  var currentIndex = array.length, temporaryValue, randomIndex;
-  // While there remain elements to shuffle...
-  while (0 !== currentIndex) {
-    // Pick a remaining element...
-    randomIndex = Math.floor(Math.random() * currentIndex);
-    currentIndex -= 1;
-    // And swap it with the current element.
-    temporaryValue = array[currentIndex];
-    array[currentIndex] = array[randomIndex];
-    array[randomIndex] = temporaryValue;
-  }
-  return array;
+function modifyCase(str) {
+  return str[0].toUpperCase() + str.substring(1).toLowerCase();
 }
 
-array = shuffle(array);
+function studentExists(firstName, lastName) {
+  return studentArray.reduce(function(exists, student) {
+    if (exists === true) {
+      return true;
+    }
+    return student[firstName] === firstName && student[lastName] === lastName;
+  }, false);
+}
+
+function addStudentOrStickers(firstName, lastName, addStickers) {
+  firstName = modifyCase(firstName);
+  lastName = modifyCase(lastName);
+  addStickers = Number(addStickers);
+  if(!studentExists(firstName, lastName)) {
+    studentArray.push(renderStudentObject(firstName, lastName, addStickers));
+  } else {
+    studentArray.forEach(function(student) {
+      if(student[firstName] === firstName && student[lastName] === lastName) {
+        student.stickers += addStickers;
+      }
+    });
+  }
+}
+
+function clickToAddStudentInfo() {
+  var firstName = $("#firstName").val();
+  var lastName = $("#lastName").val();
+  var addStickers = $("#addStickers").val();
+  console.log(firstName + ', ' + lastName + ', ' + addStickers)
+  if(firstName === undefined || !(/^[\w\-]+$/.test(firstName))) {
+    alert('Please enter a valid first name. (accepts characters a-z and hyphens)');
+    return;
+  } else if(lastName === undefined || !(/^[\w\-]+$/.test(lastName))) {
+    alert('Please enter a valid last name. (accepts characters a-z and hyphens)');
+    return;
+  } else if(addStickers === undefined || !(/[\d]+/.test(addStickers))) {
+    alert('Please enter a valid number of sticklers. (accepts digits 0-9)');
+    return;
+  } else {
+    alert('Student information successfully updated!');
+    return addStudentOrStickers(firstName, lastName, addStickers);
+  }
+}
+
+function refreshRaffleArray() {
+  raffleArray = [];
+  studentArray.slice(0).forEach(function(student) {
+    for(var i = 1; i <= student.stickers; i++) {
+      raffleArray.push(student.raffleName);
+    }
+  });
+  $("ol").html('');
+}
+
 
 function generateRan() {
-  var ol = document.getElementById("ol");
-  var li = document.createElement("li");
-  var name = document.createTextNode(array[i--]);
-  if (i >= 0){
-    li.appendChild(name);
-    ol.appendChild(li);
-    li.className = "text-center";
-  } else {
-    alert("No more names to display!");
+  if(raffleArray.length === 0) {
+    alert('No more names to display!');
+    return;
   }
-  document.getElementById("end").scrollIntoView();
+  var randomIndex = Math.floor(Math.random() * raffleArray.length);
+  var chosenStudent = raffleArray[randomIndex];
+  raffleArray.splice(randomIndex, 1);
+  var li = $("<li>" + chosenStudent + "</li>")
+  $(li).prependTo("ol");
+  $(li).addClass("text-center");
 }
+
+
+$("#updateStudentInfo").click(clickToAddStudentInfo);
+$(document).ready(refreshRaffleArray);
+$("#startOver").click(refreshRaffleArray)
